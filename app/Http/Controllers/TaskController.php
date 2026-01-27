80c6439e3d0418161ffc91ec6b529a26a0640499
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-01-27 21:46:12 +0100

    feat: index method has been updated to get the the tasks based on the latest than has been added

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,7 +12,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
+        $tasks = Task::latest()->get();
         return view('tasks.index', compact('tasks'));
     }
 
@@ -37,7 +37,7 @@ class TaskController extends Controller
             'status' => 'required|in:todo,in_progress,done',
         ]);
         Task::create($validated);
-        return redirect('tasks.index')->route('tasks.index')->with('status', 'task created successfully');
+        return redirect()->route('tasks.index')->with('status', 'task created successfully');
     }
 
     /**
@@ -45,7 +45,7 @@ class TaskController extends Controller
      */
     public function show(Task $task)
     {
-        //
+        return view('tasks.show', compact('task'));
     }
 
     /**
