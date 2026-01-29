fdd41f47440e6ad821db550d35bf236555ec0785
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-01-29 09:06:38 +0100

    feat: Task routes (including create, edit, archive & delete)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,4 +18,13 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+
+Route::prefix('tasks')->name('tasks.')->group(function () {
+    Route::get('/archived', [TaskController::class, 'archived'])->name('archived');
+    Route::patch('/{id}/restore', [TaskController::class, 'restore'])->name('restore');
+    Route::delete('/{id}/force-destroy', [TaskController::class, 'forceDestroy'])->name('forceDestroy');
+});
+,,  
+Route::resource('tasks', TaskController::class);
+
+require __DIR__ . '/auth.php';
